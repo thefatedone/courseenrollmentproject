40ecde597325c3406d5b9d5cbbc363e287512599
thefatedone <alexpepo2006@gmail.com>
2025-11-25 18:44:51 +0400

    adding test for TeacherRepository (#6)
    * adding test for StudentRepository
    
    * Added Integrational tests for all methods in TeacherServiceTest.java. Tests are working properly
    
    ---------
    
    Co-authored-by: Chingiz Akmatov <chingiz3101@gmail.com>

--- src/main/java/com/alex/enrollment/teacher/repository/TeacherRepository.java
+++ src/main/java/com/alex/enrollment/teacher/repository/TeacherRepository.java
@@ -1,10 +1,15 @@
 package com.alex.enrollment.teacher.repository;
 
+import com.alex.enrollment.student.model.Student;
 import com.alex.enrollment.teacher.model.Teacher;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeacherRepository extends CrudRepository<Teacher, Integer> {
+
+    Optional<Teacher> findByFirstNameAndLastName(String firstName, String lastName);
 
 }
